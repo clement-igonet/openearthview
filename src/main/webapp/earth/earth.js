3af055e8d943a3a51c81a5f07f327d76089050e8
Clément Igonet <clement.igonet@galaxysemi.com>
2015-05-09 01:10:25 +0200

    Use segmentSphere for earth

--- src/main/webapp/earth/earth.js
+++ src/main/webapp/earth/earth.js
@@ -7,6 +7,7 @@ document.onload = function () {
     osm2X3d.init();
     document.addEventListener('keydown', function (event) {
         if (event.keyCode == 88) { // 'x' keydown
+            x3dom.debug.doLog('update called.');
             osm2X3d.updateView();
             osm2X3d.updateScene();
         }
@@ -22,6 +23,11 @@ function Osm2X3dEarth() {
     this.camPos = new x3dom.fields.SFVec3f(0, this.elev, 0);
     this.camOri;
     this.zoom;
+//    this.guest = 0;
+//    this.camPos = new x3dom.fields.SFVec3f(0, this.elev, 0);
+//    this.geoPos = new x3dom.fields.MFVec3f();
+//    this.elev, this.lon, this.lat);
+//this.geoOps.
 }
 
 
@@ -31,7 +37,7 @@ Osm2X3dEarth.prototype.init = function () {
     navigationInfo.setAttribute('id', 'nav');
     navigationInfo.setAttribute('headlight', 'true');
     navigationInfo.setAttribute('type', 'turntable');
-    navigationInfo.setAttribute('typeParams', '0 0 0 3.13');
+    navigationInfo.setAttribute('typeParams', '0 0 0 3.0');
     navigationInfo.setAttribute('transitionType', 'TELEPORT');
 
     scene.appendChild(navigationInfo);
@@ -47,7 +53,12 @@ Osm2X3dEarth.prototype.init = function () {
     cameraTransform.setAttribute('rotation', '1 0 0 ' + rotLat);
     cameraTransform.setAttribute('translation', '0 0 0');
     cameraTransform.appendChild(self.viewpoint);
-    scene.appendChild(cameraTransform);
+    var cameraTransform2 = document.createElement('Transform');
+    cameraTransform2.setAttribute('rotation', '0 1 0 ' + rotLon);
+    cameraTransform2.setAttribute('translation', '0 0 0');
+    cameraTransform2.appendChild(cameraTransform);
+    scene.appendChild(cameraTransform2);
+//    scene.appendChild(cameraTransform);
     self.viewpoint.addEventListener("viewpointChanged", self.view_changed, false);
 
     osm2X3d.updateView();
@@ -59,6 +70,7 @@ Osm2X3dEarth.prototype.updateView = function () {
 }
 
 Osm2X3dEarth.prototype.updateScene = function () {
+    x3dom.debug.doLog('updateScene.');
     var self = this;
     var sceneTransform = document.createElement('Transform');
     sceneTransform.setAttribute('id', 'earthTransform');
@@ -70,15 +82,27 @@ Osm2X3dEarth.prototype.updateScene = function () {
     imageTexture.setAttribute('scale', 'true');
     appearance.appendChild(imageTexture);
     shape.appendChild(appearance);
-    var sphereSegment = document.createElement('SphereSegment');
+
+    x3dom.debug.doLog('guest:' + self.guest);
+    var sphereSegment;
+    var sphereSegment = document.getElementById('SphereSegment');
+    if (sphereSegment) {
+        sphereSegment.parentNode.removeChild(sphereSegment);
+    }
+
+    sphereSegment = document.createElement('SphereSegment');
+    sphereSegment.setAttribute('id', 'SphereSegment');
     var latitude = '';
-    for (i = -90; i <= 90; i += 10) {
+    var precision = 64;
+    var stepLat = 180 / precision;
+    for (i = -90; i <= 90; i += stepLat) {
         latitude += i + ' ';
     }
     sphereSegment.setAttribute('latitude', latitude);
 
     var longitude = '';
-    for (i = 360; i >= 0; i -= 10) {
+    var stepLon = 360 / precision;
+    for (i = 360; i >= -0; i -= stepLon) {
         longitude += i + ' ';
     }
     sphereSegment.setAttribute('longitude', longitude);
@@ -93,10 +117,19 @@ Osm2X3dEarth.prototype.view_changed = function (e) {
     var self = this;
     self.camPos = e.position;
     self.camOri = e.orientation;
-    x3dom.debug.doLog('camPos: ' + self.camPos, x3dom.debug.INFO);
-    x3dom.debug.doLog('camOri: ' + self.camOri, x3dom.debug.INFO);
     self.zoom = Osm2X3d.processZoom(self.camPos);
     x3dom.debug.doLog('zoom: ' + self.zoom, x3dom.debug.INFO);
+
+
+    var pos = self.camPos;
+    x3dom.debug.doLog('viewpoint position: ' + pos, x3dom.debug.INFO);
+    var rdist2 = Math.pow(pos.x, 2) + Math.pow(pos.y, 2) + Math.pow(pos.z, 2);
+    var rdist = Math.sqrt(rdist2);
+    self.lon = Math.atan2(pos.x, pos.z) * 180.0 / Math.PI + 90;
+    self.lat = Math.asin(pos.y / rdist) * 180.0 / Math.PI;
+    x3dom.debug.doLog('r: ' + rdist, x3dom.debug.INFO);
+    x3dom.debug.doLog('lon: ' + self.lon, x3dom.debug.INFO);
+    x3dom.debug.doLog('lat: ' + self.lat, x3dom.debug.INFO);
 
     if (Math.abs(self.zoom - self.zoomOld) >= 1) {
         self.zoomOld = self.zoom;
@@ -113,8 +146,10 @@ Osm2X3d.processZoom = function (camCoord) {
     var rdist2 = Math.pow(camCoord.x, 2)
             + Math.pow(camCoord.y, 2)
             + Math.pow(camCoord.z, 2);
-    var rdist = Math.sqrt(rdist2) - EARTH_RADIUS;
-    var zoom_ = parseInt(Osm2X3d.myZConst - Math.log2(rdist / 1000.0));
+    var rdist = Math.sqrt(rdist2);
+    var elevation = rdist - EARTH_RADIUS;
+    var zoom_ = parseInt(Osm2X3d.myZConst - Math.log2(elevation / 1000.0));
+    x3dom.debug.doLog('log2(elevation): ' + Math.log2(elevation / 1000.0));
     zoom_ = Math.min(zoom_, 19);
     zoom_ = Math.max(zoom_, 1);
     return zoom_;
